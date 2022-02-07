ae4f62685d92c1b884701761dc74815bedb39e9c
soohyun <bmy1320@gmail.com>
2022-02-07 16:36:52 +0900

    2022-02-07 포트폴리오 project 영역 추가, down,up btn 구현

--- script.js
+++ script.js
@@ -9,16 +9,26 @@ $(document).ready(function(){
 
     // typing animation script
     var typed = new Typed(".typing", {
-        strings: ["열정이 넘치는", "끈기가 있는", "Front-End 지망생"],
+        strings: ["열정이 넘치는", "끈기가 있는", "성장하고 싶어하는", "Front-End 지망생"],
         typeSpeed:50,
         backSpped:60,
         loop: true
     });
 
     var typed = new Typed(".typing2", {
-        strings: ["꾸준합니다.", "열정이 넘칩니다."],
-        typeSpeed:200,
+        strings: ["꾸준합니다.", "더욱더 성장하고 싶습니다.", "열정이 넘칩니다."],
+        typeSpeed:100,
         backSpped:60,
         loop: true
     });
 })
+
+// 화면 맨위로 올려주는 버튼
+function uppage(){
+    document.documentElement.scrollTop = 0;
+  }
+
+function downpage(){
+document.documentElement.scrollTop = 3180;
+}
+
